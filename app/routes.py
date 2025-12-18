b3643a95f11e2bbdeaa71bbc367307536c2221b5
farah-sellami <farah.sell451@gmail.com>
2025-12-18 12:02:57 +0100

    v2_R&D feature

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,9 @@ import joblib
 
 main = Blueprint("main", __name__)
 
-# charger les objets ML
-reg = joblib.load("models/reg.pkl")
+# charger les modèles
+reg = joblib.load("models/reg.pkl")        # modèle complet
+reg1 = joblib.load("models/reg1.pkl")      # R&D seulement
 ct = joblib.load("models/ct.pkl")
 noms = joblib.load("models/noms.pkl")
 
@@ -14,27 +15,31 @@ def index():
     prediction = None
 
     if request.method == "POST":
+        action = request.form["action"]
         rd = float(request.form["rd_spend"])
-        admin = float(request.form["administration"])
-        marketing = float(request.form["marketing_spend"])
-        state = request.form["state"]
 
-        # recréer EXACTEMENT la ligne comme dans le training
-        ligne = [rd, admin, marketing, state]
+        # ---------- CAS 1 : R&D seulement ----------
+        if action == "rd":
+            X = pd.DataFrame([[rd]], columns=['R&D Spend'])
+            prediction = reg1.predict(X)[0]
 
-        ligne_df = pd.DataFrame(
-            [ligne],
-            columns=['R&D Spend', 'Administration', 'Marketing Spend', 'State']
-        )
+        # ---------- CAS 2 : modèle complet ----------
+        else:
+            admin = float(request.form["administration"])
+            marketing = float(request.form["marketing_spend"])
+            state = request.form["state"]
 
-        # transformations identiques au training
-        ligne_df = ct.transform(ligne_df)
-        ligne_df = pd.DataFrame(ligne_df, columns=noms)
+            ligne = [rd, admin, marketing, state]
 
-        # supprimer la colonne redondante
-        ligne_df = ligne_df.iloc[:, 1:]
+            ligne_df = pd.DataFrame(
+                [ligne],
+                columns=['R&D Spend', 'Administration', 'Marketing Spend', 'State']
+            )
 
-        # prédiction
-        prediction = reg.predict(ligne_df)[0]
+            ligne_df = ct.transform(ligne_df)
+            ligne_df = pd.DataFrame(ligne_df, columns=noms)
+            ligne_df = ligne_df.iloc[:, 1:]
+
+            prediction = reg.predict(ligne_df)[0]
 
     return render_template("index.html", prediction=prediction)
